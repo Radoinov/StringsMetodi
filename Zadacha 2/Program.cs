9ab858b92e0568f66e638a1af5f1d14c31e4e841
Radoinov <radoinov08@gmail.com>
2024-03-26 23:18:01 +0200

    Domashna

---
+++ Zadacha 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace Zadacha_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+
+    }
+}
---
+++ Zadacha 3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Zadacha_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string karta = Console.ReadLine();
+
+            while (true)
+            {
+                string[] clue = Console.ReadLine().Split();
+                char searchedCharacter = char.Parse(clue[0]);
+                int minimumCount = int.Parse(clue[1]);
+
+                int index = karta.IndexOf(searchedCharacter);
+                if (index != -1)
+                {
+                    int count = 0;
+                    for (int i = index; i < karta.Length; i++)
+                    {
+                        if (karta[i] == searchedCharacter)
+                        {
+                            count++;
+                        }
+                        if (count >= minimumCount)
+                        {
+                            Console.WriteLine($"Hideout found at index {index} and it is with size {count}!");
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
